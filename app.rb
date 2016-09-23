1f97e0d08ad17799ada79f105d3332818f62690f
Troy Whorten <sabotuer99@gmail.com>
2016-09-23 20:04:27 +0000

    more autograder tweeks

--- app.rb
+++ app.rb
@@ -48,7 +48,7 @@ class HangpersonApp < Sinatra::Base
   		  flash[:message]="You have already used that letter."
       end 
     rescue
-      flash[:message]="Invalid!"
+      flash[:message]="Invalid guess."
     end
     redirect '/show'
   end
